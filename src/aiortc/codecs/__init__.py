c000e51df09da0d4a148f0b552c035bbfa32939a
Lucas <luca.monnin@gmail.com>
2022-03-24 15:32:58 +0100

    high444p profile

--- src/aiortc/codecs/__init__.py
+++ src/aiortc/codecs/__init__.py
@@ -86,7 +86,7 @@ def init_codecs() -> None:
         dynamic_pt += 2
 
     add_video_codec("video/VP8")
-    for profile_level_id in ("42001f", "42e01f"):
+    for profile_level_id in ("f4001f", "42001f", "42e01f"):
         add_video_codec(
             "video/H264",
             {
